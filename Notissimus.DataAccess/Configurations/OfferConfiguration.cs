e33e13e011d38389bf8d1c48ff17a08f6238c6fe
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-02-08 01:46:21 +0300

    feat: data access

---
+++ Notissimus.DataAccess/Configurations/OfferConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notissimus.Domain.Entities;
+
+namespace Notissimus.DataAccess.Configurations;
+
+public class OfferConfiguration : IEntityTypeConfiguration<Offer>
+{
+    public void Configure(EntityTypeBuilder<Offer> builder)
+    {
+        builder.Navigation(o => o.AdditionalProperties).HasField("_additionalProperties");
+
+        builder.HasKey(o => o.PrimaryKey);
+    }
+}
